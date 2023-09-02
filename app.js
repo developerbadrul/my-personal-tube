ee414cf0e0caf601936680a993e9cecf1175bf96
Md Badrul Alam <developer.badrul@gmail.com>
2023-09-02 11:01:06 +0600

    Add Blog Post

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+document.getElementById('blog-btn').addEventListener('click', ()=>{
+    location.href = "blog.html"
+});
+
+function goHome(){
+    location.href = "index.html";
+};
 
 const loadDate = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
